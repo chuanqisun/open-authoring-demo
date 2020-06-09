bb9aecfe97c9e432c1103033b7935bc0069a181a
Chuanqi Sun <1895289+chuanqisun@users.noreply.github.com>
2020-06-09 09:50:09 -0700

    add a fluent ui button

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import {
   GithubClient,
   TinacmsGithubProvider,
 } from "react-tinacms-github";
+import { PrimaryButton } from "office-ui-fabric-react";
 
 export default class Site extends App {
   cms: TinaCMS;
@@ -83,8 +84,12 @@ export const EditLink = ({ editMode }: EditLinkProps) => {
   const github = useGithubEditing();
 
   return (
-    <button onClick={editMode ? github.exitEditMode : github.enterEditMode}>
-      {editMode ? "Exit Edit Mode" : "Edit This Site"}
-    </button>
+    // <button onClick={editMode ? github.exitEditMode : github.enterEditMode}>
+    //   {editMode ? "Exit Edit Mode" : "Edit This Site"}
+    // </button>
+    <PrimaryButton
+      text={editMode ? "Exit Edit Mode" : "Edit This Site"}
+      onClick={editMode ? github.exitEditMode : github.enterEditMode}
+    />
   );
 };
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,23 @@
-import Document from "next/document";
+import Document, { Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
+import { Stylesheet, InjectionMode, resetIds } from "office-ui-fabric-react";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
+    // Fluent UI styles
+    const stylesheet = Stylesheet.getInstance();
+    stylesheet.setConfig({
+      injectionMode: InjectionMode.none,
+      namespace: "server",
+    });
+    stylesheet.reset();
+    resetIds();
+
+    // Styled components styles
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
+    // Merge all SSR styles
     try {
       ctx.renderPage = () =>
         originalRenderPage({
@@ -16,6 +28,9 @@ export default class MyDocument extends Document {
       const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
+        // Fluent UI
+        styleTags: stylesheet.getRules(true),
+        // Styled Components
         styles: (
           <>
             {initialProps.styles}
@@ -26,5 +41,22 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <style
+            type="text/css"
+            dangerouslySetInnerHTML={{ __html: (this.props as any).styleTags }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
   }
 }
