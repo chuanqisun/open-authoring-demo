6265969e0deb1a0a7a48fdb76620d67775adb6db
Chuanqi Sun <1895289+chuanqisun@users.noreply.github.com>
2020-06-08 21:37:29 -0700

    add a second page

---
+++ pages/styles/color.tsx
@@ -0,0 +1,62 @@
+import Head from "next/head";
+/**
+ * Import helpers and GetStaticProps type
+ */
+import { getGithubPreviewProps, parseJson } from "next-tinacms-github";
+import { GetStaticProps } from "next";
+import {
+  useGithubJsonForm,
+  useGithubToolbarPlugins,
+} from "react-tinacms-github";
+import { usePlugin } from "tinacms";
+
+export default function StylesColor({ file }) {
+  const formOptions = {
+    label: "Styles Color",
+    fields: [{ name: "title", component: "text" }],
+  };
+
+  const [data, form] = useGithubJsonForm(file, formOptions);
+  usePlugin(form);
+
+  useGithubToolbarPlugins();
+
+  return (
+    <div className="container">
+      <Head>
+        <title>HITS Deisng Lab</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1 className="title">{data.title}</h1>
+      </main>
+    </div>
+  );
+}
+
+/**
+ * Fetch data with getStaticProps based on 'preview' mode
+ */
+export const getStaticProps: GetStaticProps = async function ({
+  preview,
+  previewData,
+}) {
+  if (preview) {
+    return getGithubPreviewProps({
+      ...previewData,
+      fileRelativePath: "content/home.json",
+      parse: parseJson,
+    });
+  }
+  return {
+    props: {
+      sourceProvider: null,
+      error: null,
+      preview: false,
+      file: {
+        fileRelativePath: "content/home.json",
+        data: (await import("../../content/home.json")).default,
+      },
+    },
+  };
+};
